6e50579699a5d48b09df014ae874ebd7061d4dc5
saurav-kattel <kattelsaurav32@gmail.com>
2024-03-09 16:37:43 +0545

    added increment and drecement features for cart items

--- shoponline/src/componentskobau/Navbar components/Addtocart.jsx
+++ shoponline/src/componentskobau/Navbar components/Addtocart.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useSelector,useDispatch } from "react-redux";
-import { cart,reset,deletecart} from "../../features/Laptopslice";
+import { useSelector, useDispatch } from "react-redux";
+import { cart, reset, deletecart, decreaseItemQuantity, addtocart } from "../../features/Laptopslice";
 import Navbar from "./Navbar";
 import Navabarup from "./Navabarup";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus,faMinus,faHeart ,faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faMinus, faHeart, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const Addtocart = () => {
   // getting the array of laptops detail which is stored in array of reducer
@@ -42,40 +42,33 @@ const Addtocart = () => {
   }, [selector]);
   console.log(imgUrls);
 
-const [price,setprice]=useState(0);
-useEffect(()=>{
-  if (selector && selector.length > 0){
-    try{
-      let totalPrice = 0;
-      selector.forEach((data) => {
-        // removes the commas and replace by white space and trim removes the white space
-        const formattedPrice = data.price.replace(/,/g, '').trim();
-        // Check if data.price is a valid number before adding it to totalPrice
-        // NaN fro not a number 
-        if (!isNaN(formattedPrice)) {
-          totalPrice += parseInt(formattedPrice);
-        } else {
-          console.warn("Invalid price for item:", data.name);
+  const [price, setprice] = useState(0);
+  useEffect(() => {
+    if (selector && selector.length > 0) {
+      try {
+        let totalPrice = 0;
+
+        for (let item of selector) {
+          totalPrice += item.price * item.quantity
         }
-      });
-      setprice(totalPrice);
-    }catch(error){
-      console.error("Error fetching images:", error);
+        setprice(totalPrice);
+      } catch (error) {
+        console.error("Error fetching images:", error);
+      }
     }
-  }
-},[selector])
+  }, [selector])
 
 
   //for resetting the array in reducer 
-const dispatch=useDispatch();
-const handlereset=()=>{
-  dispatch(reset());
-}
+  const dispatch = useDispatch();
+  const handlereset = () => {
+    dispatch(reset());
+  }
 
-// dispatches the action which is deleltecart
-const hanledeleteitem=(e)=>{
-  dispatch(deletecart(e))
-}
+  // dispatches the action which is deleltecart
+  const hanledeleteitem = (e) => {
+    dispatch(deletecart(e))
+  }
 
 
   return (
@@ -100,11 +93,11 @@ const hanledeleteitem=(e)=>{
                           className="bg-image"
                           data-mdb-ripple-color="light">
                           {imgUrls[index] &&
-                          <img
-                            src={imgUrls[index]}
-                            className="w-48"
-                            alt="Blue Jeans Jacket"
-                          />}
+                            <img
+                              src={imgUrls[index]}
+                              className="w-48"
+                              alt="Blue Jeans Jacket"
+                            />}
                         </div>
                       </div>
 
@@ -119,7 +112,7 @@ const hanledeleteitem=(e)=>{
                           className="bg-blue-600 p-2 text-white  me-5 mb-2  rounded-md"
                           data-mdb-toggle="tooltip"
                           title="Remove item">
-                          <i className="fas fa-trash" onClick={()=>hanledeleteitem(index)}><FontAwesomeIcon icon={faTrash} /></i>
+                          <i className="fas fa-trash" onClick={() => hanledeleteitem(index)}><FontAwesomeIcon icon={faTrash} /></i>
                         </button>
                         <button
                           type="button"
@@ -134,16 +127,18 @@ const hanledeleteitem=(e)=>{
                         <div className="flex flex-row mb-4" style={{ maxWidth: "300px" }}>
                           ``
                           <button
+                            onClick={() => { dispatch(decreaseItemQuantity(datas)) }}
                             className="p-1 bg-blue-600 px-3 me-2  rounded-md"
-                           >
+                          >
                             <i><FontAwesomeIcon icon={faMinus} /></i>
                           </button>
                           <div className="form-outline">
                             <label className="form-label" htmlFor="form1">
-                              Quantity
+                              Quantity {datas.quantity}
                             </label>
                           </div>
                           <button
+                            onClick={() => { dispatch(addtocart(datas)) }}
                             className="p-1 bg-blue-600 px-3 ms-2  rounded-md"
                           >
                             <i><FontAwesomeIcon icon={faPlus} /></i>
@@ -228,7 +223,7 @@ const hanledeleteitem=(e)=>{
                   </button>
                 </div>
               </div>
-                  <button type="button" onClick={handlereset} className="p-2 bg-red-500 text-white rounded-md ml-24 mt-4 "> Reset</button>
+              <button type="button" onClick={handlereset} className="p-2 bg-red-500 text-white rounded-md ml-24 mt-4 "> Reset</button>
             </div>
           </div>
         </div>
--- shoponline/src/features/Laptopslice.js
+++ shoponline/src/features/Laptopslice.js
@@ -1,96 +1,126 @@
-import {createSlice,createAsyncThunk} from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const initialState ={
-loading: false,
-    error: null,
-    laptopData: null,
-    laptopdatabyid:null,
-    cart:[],
+const initialState = {
+  loading: false,
+  error: null,
+  laptopData: [],
+  laptopdatabyid: null,
+  cart: [],
 }
 
 // fetching the filtred data of laptop in redux using asyncthunk
 export const fetchlaptop = createAsyncThunk('get/fetchfilteredlaptop', async (type) => {
   try {
-      const response = await fetch(`http://localhost:5000/api/admin/filtered-data?type=${type}`);
-      if (!response.ok) {
-          throw new Error('Failed to fetch');
-      }
-      return await response.json();
+    const response = await fetch(`http://localhost:5000/api/admin/filtered-data?type=${type}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch');
+    }
+    return await response.json();
   } catch (error) {
-      console.error('Error fetching laptop data:', error);
-      throw error;
+    console.error('Error fetching laptop data:', error);
+    throw error;
   }
 });
 
 // fetch the data by id
-  export const fetchlaptopbyid = createAsyncThunk(`laptop/fetchlaptopbyid`, async (laptopid) => {
-    const response = await fetch(`http://localhost:5000/api/admin/laptop/${laptopid}`)
-    return await  response.json()
-  })
+export const fetchlaptopbyid = createAsyncThunk(`laptop/fetchlaptopbyid`, async (laptopid) => {
+  const response = await fetch(`http://localhost:5000/api/admin/laptop/${laptopid}`)
+  return await response.json()
+})
+
+
+function addToCart(state, payload) {
+  const existingItem = state.find(item => item._id === payload._id);
+
+  if (existingItem) {
+    return state.map(item =>
+      item._id === payload._id ? { ...item, quantity: item.quantity + 1 } : item
+    );
+  } else {
+    return [...state, { ...payload }];
+  }
+}
+
+function decreaseQunaity(state, payload) {
+  const existingItem = state.find(item => item._id === payload._id);
+  if (existingItem.quantity === 1) {
+    return state.filter((item) => item._id !== existingItem._id)
+  }
+  return state.map((item) => item._id === existingItem._id ? { ...item, quantity: item.quantity - 1 } : item)
+}
+
 
 export const Laptopslice = createSlice({
-    name: "laptopslice",
-    initialState,
-    reducers: {
-      addtocart:(state,action)=>{
-        return {
-          ...state,
-          cart: [...state.cart, action.payload] // Add the payload to the cart array
-        };
-      },
-      reset:(state,action)=>{
-        return {...state,cart:[]}
-      },
-      deletecart:(state,action)=>{
-        const index=action.payload;
-        return {...state,
-        cart:[
-          ...state.cart.slice(0, index),
-          ...state.cart.slice(index + 1),
-        ]}
+  name: "laptopslice",
+  initialState,
+  reducers: {
+    addtocart: (state, action) => {
+      return {
+        ...state,
+        cart: addToCart(state.cart, action.payload)// Add the payload to the cart array
+      };
+    },
+    decreaseItemQuantity: (state, action) => {
+      return {
+        ...state,
+        cart: decreaseQunaity(state.cart, action.payload)
       }
     },
-    // extrareducers allows you to handle actions from other slices or additional reducer logic that doesn't belong 
-    // to a specific action creator defined within the slice.
-    extraReducers: (builder) => {
-        builder
-        // .addCase() is a method provided by Redux Toolkit's createSlice 
-        // function for defining reducer logic. It allows you to define how your slice's
-        //  state should be updated in response to specific actions.
-          .addCase(fetchlaptop.pending, (state) => {
-            state.loading = true;
-            state.error = null;
-          })
-          .addCase(fetchlaptop.fulfilled, (state, action) => {
-            state.loading = false;
-            state.error = null;
-            state.laptopData = action.payload;
-          })
-          .addCase(fetchlaptop.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.error.message;
-          })
-          .addCase(fetchlaptopbyid.pending, (state) => {
-            state.loading = true;
-            state.error = null;
-          })
-          .addCase(fetchlaptopbyid.fulfilled, (state, action) => {
-            state.loading = false;
-            state.error = null;
-            state.laptopdatabyid = action.payload;
-          })
-          .addCase(fetchlaptopbyid.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.error.message;
-          });
+    reset: (state, action) => {
+      return { ...state, cart: [] }
+    },
+    deletecart: (state, action) => {
+      const index = action.payload;
+      return {
+        ...state,
+        cart: [
+          ...state.cart.slice(0, index),
+          ...state.cart.slice(index + 1),
+        ]
       }
+    }
+  },
+  // extrareducers allows you to handle actions from other slices or additional reducer logic that doesn't belong 
+  // to a specific action creator defined within the slice.
+  extraReducers: (builder) => {
+    builder
+      // .addCase() is a method provided by Redux Toolkit's createSlice 
+      // function for defining reducer logic. It allows you to define how your slice's
+      //  state should be updated in response to specific actions.
+      .addCase(fetchlaptop.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchlaptop.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.laptopData = action.payload;
+      })
+      .addCase(fetchlaptop.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      .addCase(fetchlaptopbyid.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchlaptopbyid.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.laptopdatabyid = action.payload;
+      })
+      .addCase(fetchlaptopbyid.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  }
 });
 
-export const laptopdata=(state)=>state.laptopslice.laptopData;
-export const  laptopError=(state)=>state.laptopslice.error;
-export  const  laptopLoading=(state)=>state.laptopslice.loading;
-export const laptopdataid=(state)=>state.laptopslice.laptopdatabyid;
-export const cart=(state)=>state.laptopslice.cart;
+export const laptopdata = (state) => state.laptopslice.laptopData;
+export const laptopError = (state) => state.laptopslice.error;
+export const laptopLoading = (state) => state.laptopslice.loading;
+export const laptopdataid = (state) => state.laptopslice.laptopdatabyid;
+export const cart = (state) => state.laptopslice.cart;
 // The function below is called a selector and returns a value based on the current state of the store.
-export const {addtocart,reset,deletecart}=Laptopslice.actions
+export const { addtocart, reset, deletecart, decreaseItemQuantity } = Laptopslice.actions
 export default Laptopslice.reducer;
